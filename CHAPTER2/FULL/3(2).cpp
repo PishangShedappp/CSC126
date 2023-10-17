d39525cbed6aed29aa0824f1f03f178abe69641d
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-10-17 18:14:02 +0800

    Create 3(2).cpp

---
+++ CHAPTER2/FULL/3(2).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int first = 0;
+	int second;
+	int third;
+	int multiply;
+	int devide;
+	
+	cout << "\nSecond variable: ";
+	cin >> second;
+	cout << "\nThird variable: ";
+	cin >> third;
+	multiply = third*7;
+	devide = multiply/second;
+	first = devide;
+	cout << first;
+}
