551187c6af7af14a7111927c4ed40ffff0d1356b
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-10-18 09:47:57 +0800

    Update 4.cpp

--- CHAPTER2/FULL/4.cpp
+++ CHAPTER2/FULL/4.cpp
@@ -7,13 +7,12 @@ int main()
   int n1, n2, n3;
   double average;
 
-  cout << "n1 = ";
+  cout << "Enter value for n1 = ";
   cin >> n1;
-  cout << "n2 = ";
+  cout << "Enter value for n2 = ";
   cin >> n2;
-  cout << "n3 = ";
+  cout << "Enter value for n3 = ";
   cin >> n3;
-  average = n1+n2+n3;
-  average = average/3;
-  cout << average;
+  average = (n1+n2+n3)/3;
+  cout << "Average is: " << average;
 }
