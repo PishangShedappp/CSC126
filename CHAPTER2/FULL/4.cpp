698bb83d0d864aa90fa9dcd49ea6bd87f9a41b10
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-10-18 09:31:41 +0800

    Create 4.cpp

---
+++ CHAPTER2/FULL/4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n1, n2, n3;
+  double everage;
+
+  cout << "n1 = ";
+  cin >> n1;
+  cout << "n2 = ";
+  cin >> n2;
+  cout << "n3 = ";
+  cin >> n3;
+  average = n1+n2+n3;
+  average = average/3;
+  cout << average;
+}
