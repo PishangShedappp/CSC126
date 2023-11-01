744b634960fb541a5c867d464b4377bb2bbfdcf6
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-11-01 08:19:30 +0800

    Create 1.cpp

---
+++ CHAPTER2_LAB2/1.cpp
@@ -0,0 +1,23 @@
+```cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int first_number, second_number;
+
+  cout << "Please enter first number:     ";
+  cin >> first_number;
+  cout << "Please enter second number:    ";
+  cin >> second_number;
+  cout << endl;
+
+  cout << "Result for the following process as show below:" << endl;
+  cout << "-----------------------------------------------" << endl;
+  cout << endl;
+  cout << "   1) Addition : " << first_number << " + " << second_number << " = " << first_number+second_number << endl;
+  cout << "   2) Substraction : " << first_number << " - " << second_number << " = " << first_number-second_number << endl;
+  cout << "   3) Multiplication : " << first_number << " * " << second_number << " = " << first_number*second_number << endl;
+  cout << "   4) Division : " << first_number << " / " << second_number << " = " << first_number/second_number << endl;
+}
+```
