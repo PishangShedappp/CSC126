5cceab0b8427d721fc4ecd286c09007b9d592040
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-11-07 16:27:57 +0800

    Create gender.cpp

---
+++ CHAPTER3/gender.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char g;
+  
+  cout << "Enter your gender: ";
+  cin >> g;
+
+  if (g == 'f') {
+    cout << "Female";
+  } 
+  else {
+    cout << "Male";
+  }
+}
