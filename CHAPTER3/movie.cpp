e6032ae11f0942c4339add5ba484289e1c8129c9
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-11-07 17:05:15 +0800

    Create movie.cpp

---
+++ CHAPTER3/movie.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string movietitle;
+  char paymentcode;
+  int moviecode, numberofticket;
+  float newprice;
+
+  cout << "Enter movie code: ";
+  cin >> moviecode;
+  cout << "Enter number of ticket: ";
+  cin >> numberofticket;
+
+  if (moviecode == 1) {
+    newprice = numberofticket * 25;
+    movietitle = "Spiderman";
+  }
+  else {
+    newprice = numberofticket * 20;
+    movietitle = "Frozen";
+  }
+
+  cout << "Enter payment code: ";
+  cin >> paymentcode;
+  if (paymentcode == 'C') {
+    newprice = newprice*1.10;
+  }
+
+  cout << "Movie : RM" << movietitle << endl;
+  cout << "Price : RM" << newprice;
+}
