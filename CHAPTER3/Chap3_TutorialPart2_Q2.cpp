ce4a7d23e2e858b1e757c794e505f7ebbcad36cf
PishangShedappp <62550064+PishangShedappp@users.noreply.github.com>
2023-11-22 08:03:24 +0800

    Create Chap3_TutorialPart2_Q2.cpp

---
+++ CHAPTER3/Chap3_TutorialPart2_Q2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n1, n2;
+  string op;
+
+  cout << "Enter your input (Ex: 8 4): ";
+  cin >> n1 >> n2;
+
+  if (n1 <= 0 || n2 <= 0) {
+    cout << "Error, number must more than 0";
+  } 
+  else {
+    cout << "Enter your operator: ";
+    cin >> op;
+
+    if (op == "+") {
+      cout << n1 + n2; 
+    }
+    else if (op == "-") {
+      cout << n1 - n2; 
+    }
+    else if (op == "*") {
+      cout << n1 * n2; 
+    }
+    else if (op == "/") {
+      cout << n1 / n2; 
+    }
+    else {
+      cout << "Operator undefined";
+    }
+  }
+}
